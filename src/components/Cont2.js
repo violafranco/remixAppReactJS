6be9e4c6ba7f2f072ef040c2ff479ba27b4ddba4
violafranco <franco5viola@gmail.com>
2022-02-14 16:32:41 -0300

    finish cont2

--- src/components/Cont2.js
+++ src/components/Cont2.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Title, Paragraph } from './Cont1';
-import circle from './../images/circle-arrow.svg'
+import square from './../images/collaboration.png';
 
 const Cont2 = () => {
     return ( 
@@ -13,11 +13,14 @@ const Cont2 = () => {
                 <Paragraph>
                 With Remix, you will be able to actively collaborate with developers in real-time. No more back-and-forth emails and Slack messages to get that design perfect.
                 </Paragraph>
-                <Button>Learn More</Button>
+                <Button>Learn More ⇨</Button>
             </ContenedorTxt>
 
             <ContenedorPhoto>
-
+                <img 
+                    src={square}
+                    style={{width: '45%', height: '40%', position: 'relative', bottom: '17.5em', left: '50%'}}
+                />
             </ContenedorPhoto>
         </Contenedor>
     );
@@ -33,26 +36,21 @@ const ContenedorTxt = styled.div`
 `
 
 const Button = styled.a`
+
     width: 13%;
     cursor: pointer;
-    font-weight: bold;
-    transition: all .2s;
-    padding: 10px 20px;
-    border-radius: 100px;
-    background: ${({theme}) => theme.BgSecondary};
-    color: ${({theme}) => theme.TextSecondary};
-    border: 1px solid transparent;
-    display: flex;
-    align-items: center;
-    font-size: 15px;
+    margin-left: 8px;
+    background-image: linear-gradient(#b38f00, #b38f00);
+    background-repeat: no-repeat;
+    background-size: 0% 100%;
+    background-position-x: right;
+    animation: all 1s;
+    transition: background-size 500ms;
 
     &:hover{
-        background: #e0b819;
-    }
-
-    &:before{
-        content: url(${circle});
-        ;
+        background-size: 100% 100%;
+        background-position-x: left;
+        transform: scale(0.7);
     }
     
 `
